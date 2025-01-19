e943c34297f3ca810a9db7ba78bbee2aa0c756ff
zennolux <zennolux@gmail.com>
2025-01-19 23:42:37 +0800

    chore: update icon

--- zebar/src/App.tsx
+++ zebar/src/App.tsx
@@ -149,7 +149,7 @@ function App() {
         {output.media?.currentSession && (
           <div className="cursor-pointer">
             <i
-              className="nf nf-md-gesture_two_tap text-gray-400"
+              className="nf nf-iec-toggle_power text-gray-400"
               onClick={() => setShowMediaInfo(!showMediaInfo)}
             ></i>
           </div>
