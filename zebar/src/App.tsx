aa8d809cfacb0ec2e39ecfff01c5313c79115b87
zennolux <zennolux@gmail.com>
2025-03-03 22:35:56 +0800

    chore: update color

--- zebar/src/App.tsx
+++ zebar/src/App.tsx
@@ -30,7 +30,7 @@ function getBatteryIcon(chargePercent: number) {
     return <i className="nf nf-fa-battery_2 text-yellow-800"></i>;
   } else if (chargePercent > 30 && chargePercent < 50) {
     return <i className="nf nf-fa-battery_1 text-yellow-600"></i>;
-  } else if (chargePercent > 50 && chargePercent < 100) {
+  } else if (chargePercent > 50 && chargePercent < 80) {
     return <i className="nf nf-fa-battery_3 text-green-200"></i>;
   } else {
     return <i className="nf nf-fa-battery_4 text-green-400"></i>;
